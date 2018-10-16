ede6f2dbd5ba6e2e6cee3ea7387efc818628ebf6
Luca Leonardo Scorcia <lucaleonardo.scorcia@mise.gov.it>
2018-10-16 15:46:51 -0400

    Rendi funzionante il comando Logout nella UI

---
+++ AngularJsWebApp.Web/coreui/js/controllers/authWidgetController.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+angular
+    .module('app').controller('authWidgetController', ['$scope', '$state', 'authService',
+        function ($scope, $state, authService) {
+
+            $scope.logOut = function () {
+                $state.transitionTo('/logout');
+            }
+
+            $scope.authentication = authService.authentication;
+
+        }]);
