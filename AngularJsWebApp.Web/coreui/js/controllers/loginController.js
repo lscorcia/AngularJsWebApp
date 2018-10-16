6ddfa29ac6884b1a517dc78e2d792d86d4542639
Luca Leonardo Scorcia <lucaleonardo.scorcia@mise.gov.it>
2018-10-16 11:08:59 -0400

    CoreUI: Rendi funzionante la pagina di login

---
+++ AngularJsWebApp.Web/coreui/js/controllers/loginController.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+angular
+    .module('app')
+    .controller('loginController', ['$scope', '$location', 'authService', function ($scope, $location, authService) {
+
+    $scope.loginData = {
+        userName: "",
+        password: ""
+    };
+
+    $scope.message = "";
+
+    $scope.login = function () {
+
+        authService.login($scope.loginData).then(function (response) {
+
+                $location.path('/orders');
+
+            },
+            function (err) {
+                if (err.data)
+                    $scope.message = err.data.error_description;
+                else
+                    $scope.message = err.status + ' - ' + err.statusText;
+            });
+    };
+
+}]);
